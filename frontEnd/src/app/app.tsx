352a62b73721147de2a0202e3c3e1e2a51253f20
CamCam <themast3t@gmail.com>
2025-04-02 01:56:02 +0700

    base

---
+++ frontEnd/src/app/app.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import React from 'react';
+// import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './globals.css';
+import LayoutWrap from './component/LayoutWrap';
+import Home from './pages/home';
+
+
+function App() {
+    return (
+      <BrowserRouter>
+        <LayoutWrap auth={false}>
+          <Routes>
+            <Route index element={<Home />} />
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </LayoutWrap>
+      </BrowserRouter>
+    );
+  }
+  
+  export default App;
